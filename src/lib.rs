9c6bb89b9e746067e8289bd09d220afed1a6abab
AliAghaahmadi <itsaliaghaahmadi@gmail.com>
2025-07-06 18:11:02 +0330

    rewrote

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ mod weighted;
 mod matcher;
 mod custom;
 mod composite;
+mod metrics;
 
 pub use core::*;
 pub use weighted::*;
 pub use matcher::*;
 pub use custom::*;
 pub use composite::*;
+pub use metrics::*;
